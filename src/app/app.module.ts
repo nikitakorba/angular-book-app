9c181b5c610f188ee1e7d23c1a973bb9f4f1336c
Nikita Korba <no.korba@gmail.com>
2018-01-03 18:24:45 +0200

    Added Navigation bar, created simple routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {appRouting} from './app.routing';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BookListComponent } from './book-list/book-list.component';
+import {CollapseModule} from 'ngx-bootstrap';
+import { AuthorListComponent } from './author-list/author-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    BookListComponent,
+    AuthorListComponent
   ],
   imports: [
     BrowserModule,
-    appRouting
+    appRouting,
+    CollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,11 @@
 import { Routes, RouterModule} from '@angular/router';
 import { ModuleWithProviders} from '@angular/core';
+import {BookListComponent} from './book-list/book-list.component';
+import {AuthorListComponent} from './author-list/author-list.component';
 
 const appRoutes: Routes = [
-
+  {path: 'booklist', component: BookListComponent},
+  {path: 'authorlist', component: AuthorListComponent}
 ];
 
 export  const appRouting: ModuleWithProviders = RouterModule.forRoot(appRoutes);
